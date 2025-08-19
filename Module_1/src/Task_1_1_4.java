2be63682fb80c957777f6a3adcb379aa137905c0
taif <taifjalo0@gmail.com>
2025-08-19 18:10:25 +0300

    up

--- Module_1/src/Task_1_1_4.java
+++ Module_1/src/Task_1_1_4.java
@@ -1,3 +1,9 @@
+/*
+Run the program. Then, based on your previous programming experience,
+modify the main method so that two cat objects are created,
+and the program behaves in the following way:
+ */
+
 public class Task_1_1_4 {
     private String name;
 
@@ -12,8 +18,15 @@ public class Task_1_1_4 {
     public static void main(String[] args) {
         // Create an instance of the Cat class with a name
         Task_1_1_4 cat = new Task_1_1_4("Whiskers");
+        Task_1_1_4 cat_2 = new Task_1_1_4("Rex");
+
+        for (int i =0; i < 1; i++) {
+            cat.meow();
+        }
 
         // Call the meow method on the cat instance
+        cat.meow();
+        cat_2.meow();
         cat.meow();
     }
 }
