24f5df9e98d2686024fa2f10a1c9e453222f973a
taif <taifjalo0@gmail.com>
2025-09-09 11:16:17 +0300

    up

--- 1.3_Probability_Distributions/src/ArrivalProcess.java
+++ 1.3_Probability_Distributions/src/ArrivalProcess.java
@@ -1,22 +1,19 @@
-import java.util.Random;
+import distributions.Distributions;
 
 class ArrivalProcess {
 
     private String eventType;
-    private Random randomGenerator;
+    private Distributions randomGenerator = new Distributions();
     private double lambda;  // rate parameter for exponential distribution
 
     public ArrivalProcess(String eventType, double lambda) {
         this.eventType = eventType;
-        this.lambda = lambda;
-        this.randomGenerator = new Random();
-    }
+        this.lambda = lambda;}
 
     // this is the main func our method to make or generate the next event and add it to the event list.
     public Event generateEvent() {
         Clock clock = Clock.getInstance();                    // get the Clock Class with singleton instance. calling for clock static instance
-        double interval = -Math.log(1 - randomGenerator.nextDouble()) / lambda; // sample an exponential interval to generate random number ln(1-U)/λ. here we have "U = randomGenerator" and " λ = lambda"
-        double eventTime = clock.getTime() + interval;       // compute absolute event time (current + interval). both times together which is 0.0 and RandomGenerter
+        double eventTime = clock.getTime() + randomGenerator.negexp(5.0);       // compute absolute event time (current + interval). both times together which is 0.0 and RandomGenerter
         clock.setTime(eventTime);                            // advance the simulation clock to the event time, after we got bith times we to setTime() by clock  from the var eventTime.
         return new Event(eventType, eventTime);              // create and return the new Event object, which is eventType and eventTime
     }
--- 1.3_Probability_Distributions/src/Customer.java
+++ 1.3_Probability_Distributions/src/Customer.java
@@ -1,6 +1,4 @@
-public class Customer {
-
-    // Task 7: Costumer arrival and departure time
+public class Customer implements Comparable<Customer> {
     private double arrivalTime;
     private double departureTime;
 
@@ -9,29 +7,29 @@ public class Customer {
         this.departureTime = departureTime;
     }
 
-    // Setters (useful if we only know arrival at first, and later we set departure)
-    public void setDepartureTime(double departureTime) {
-        this.departureTime = departureTime;
-    }
-
-    // Getters
     public double getArrivalTime() {
         return arrivalTime;
     }
+
     public double getDepartureTime() {
         return departureTime;
     }
 
-    // Calculate time customer spent in the system: getCustomerSpentTime = departureTime − arrivalTime
-    // (difference between arrival time and customer departure time) is the time each customer spent in the system.
+    public void setDepartureTime(double departureTime) {
+        this.departureTime = departureTime;
+    }
+
     public double getCustomerSpentTime() {
         return departureTime - arrivalTime;
     }
 
     @Override
+    public int compareTo(Customer other) {
+        return Double.compare(this.arrivalTime, other.arrivalTime);
+    }
+
+    @Override
     public String toString() {
-        return "Customer [Arrival=" + arrivalTime +
-                ", Departure=" + departureTime +
-                ", Spent=" + getCustomerSpentTime() + "]";
+        return "Customer{arrival=" + arrivalTime + ", departure=" + departureTime + "}";
     }
 }
