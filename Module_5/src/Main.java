a2f1e61b77c021204a0f4333e149ff5228c60762
taif <taifjalo0@gmail.com>
2025-09-10 21:50:20 +0300

    up

--- Module_5/src/Main.java
+++ Module_5/src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class A extends Thread {
+    public void run() {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 != 0) {
+                System.out.println("Odd Thread: " + i);
+            }
         }
     }
 }
+
+class B extends Thread {
+    public void run() {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Even Thread: " + i);
+            }
+        }
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            A objOdd = new A();
+            B objEven = new B();
+
+            objEven.start();    // start() method is used to start the thread by calling the run() method
+            objEven.join();     // join() method is used to wait for a thread to die. In other words, it makes the current thread to pause execution until the thread it joins with completes its execution.
+
+            objOdd.start();
+            objOdd.join();
+
+            System.out.println("Printing complete.");
+
+        } catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+}
+
+
+
+
+/*
+class OddOrEven extends Thread {
+    public void run() {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 != 0) {
+                System.out.println("Odd Thread: " + i);
+            } else {
+                System.out.println("Even Thread: " + i);
+            }
+        }
+    }
+}
+public class Main extends Thread  {
+    public static void main(String[] args) {
+
+        OddOrEven obj = new OddOrEven();
+        obj.start();
+
+
+    }
+}
+*/
+
