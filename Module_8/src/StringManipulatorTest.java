57e0db27af4f5d9ea2618edab7c1a60f1948be27
taif <taifjalo0@gmail.com>
2025-09-27 21:40:37 +0300

    8.1

--- Module_8/src/StringManipulatorTest.java
+++ Module_8/src/StringManipulatorTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class StringManipulatorTest {
 
     @Test
@@ -9,35 +8,40 @@ public class StringManipulatorTest {
         StringManipulator sm = new StringManipulator();
         String result = sm.concatenate("Hello, ", "World!");
         Assertions.assertEquals("Hello, World!", result);
-
     }
 
     @Test
-    void canConcatenateStrings() {
+    void findLengthTest() {
         StringManipulator sm = new StringManipulator();
         int result = sm.findLength("Hello");
         Assertions.assertEquals(5, result);
-
     }
 
     @Test
-    void convertToUpperCase() {
+    void convertToUpperCaseTest() {
         StringManipulator sm = new StringManipulator();
         String result = sm.convertToUpperCase("hello");
         Assertions.assertEquals("HELLO", result);
     }
 
     @Test
-    void convertToLowerCase() {
+    void convertToLowerCaseTest() {
         StringManipulator sm = new StringManipulator();
         String result = sm.convertToLowerCase("HELLO");
         Assertions.assertEquals("hello", result);
     }
 
     @Test
-    void containsSubstring() {
+    void containsSubstringTest() {
         StringManipulator sm = new StringManipulator();
         boolean result = sm.containsSubstring("Hello, World!", "World");
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    void doesNotContainSubstringTest() {
+        StringManipulator sm = new StringManipulator();
+        boolean result = sm.containsSubstring("Hello, World!", "Java");
+        Assertions.assertFalse(result);
     }
 }
